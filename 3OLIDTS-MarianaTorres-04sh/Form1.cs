e7e3b1314cf8a34655da112d0b7fe28407e5037b
Mariana Torres <mardecar44@outlook.com>
2025-10-12 00:45:28 -0600

    rama 3

--- 3OLIDTS-MarianaTorres-04sh/Form1.cs
+++ 3OLIDTS-MarianaTorres-04sh/Form1.cs
@@ -3,70 +3,194 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO; //libreia para lectura y escitura de archivos
+using System.IO; //Libreria para lectura y escritura de archivos
+using System.Text.RegularExpressions; // Libreria para analisis de formatod de texto
+using MySql.Data.MySqlClient; // Libreria externa para la conexión a la base de datos
 
 namespace _3OLIDTS_MarianaTorres_04sh
 {
     public partial class Form1 : Form
     {
+        string ConexionSQL = "Server=127.0.0.1;Port=3306;Database=formulario3o;Uid=root;Pwd=mardecartorlo44;";
         public Form1()
         {
             InitializeComponent();
+            tbedad.TextChanged += validarEdad;
+            tbestatura.TextChanged += validarEstatura;
+            tbtelefono.Leave += validarTelefono;
+            tbnombre.TextChanged += validarNombre;
+            tbapellidos.TextChanged += validarApellidos;
+        }
+        private void InsertarRegistro(string nombre, string apellidos, int edad, string genero, decimal estatura, string telefono)
+        {
+            using (MySqlConnection conn = new MySqlConnection(ConexionSQL))
+            {
+                conn.Open();
+                string insertQuery = "INSERT INTO registro (nombre, apellidos, Telefono,estatura, edad, genero) " +
+                    "VALUES(@Nombre, @Apellidos, @Telefono,@Estatura, @Edad, @Genero)";
+                using (MySqlCommand comando = new MySqlCommand(insertQuery, conn))
+                {
+                    comando.Parameters.AddWithValue("@Nombre", nombre);
+                    comando.Parameters.AddWithValue("@Apellidos", apellidos);
+                    comando.Parameters.AddWithValue("@Telefono", telefono);
+                    comando.Parameters.AddWithValue("@Estatura", estatura);
+                    comando.Parameters.AddWithValue("@Edad", edad);
+                    comando.Parameters.AddWithValue("@Genero", genero);
+                    comando.ExecuteNonQuery(); 
+                }
+                conn.Close();
+            }
         }
 
         private void lim_Click(object sender, EventArgs e)
         {
-            tbnombre.Clear();
             tbapellidos.Clear();
-            tbtelefono.Clear();
+            tbnombre.Clear();
             tbestatura.Clear();
+            tbtelefono.Clear();
             tbedad.Clear();
             rb.Checked = false;
             rb2.Checked = false;
         }
 
+
+
         private void gua_Click(object sender, EventArgs e)
         {
-            string apellidos = tbapellidos.Text;
             string nombre = tbnombre.Text;
-            string telefono = tbtelefono.Text;
+            string apellidos = tbapellidos.Text;
             string edad = tbedad.Text;
             string estatura = tbestatura.Text;
+            string telefono = tbtelefono.Text;
+
             string genero = "";
-
-            if (rb2.Checked)
+            if (rb.Checked)
             {
-                genero = "Femenino";
+                genero = "Hombre";
             }
-            else if (rb.Checked)
+            else if (rb2.Checked)
             {
-                genero = "Masculino";
+                genero = "Mujer";
             }
 
-            string datos = $"Nombre: {nombre}\r\nApellidos: {apellidos}\r\n" +
-                           $"Teléfono: {telefono}\r\nEstatura: {estatura}\r\n" +
-                           $"Edad: {edad}\r\nGénero: {genero}";
-              string ruta = "C:\\Users\\marde\\Downloads\\nada.txt";
-            // string ruta = "C:\\Users\\marde\\Downloads";
-            // string ruta = @"C:\Users\mariana torres\Downloads";
-            bool archivoExiste = File.Exists(ruta);
-            using (StreamWriter writer = new StreamWriter(ruta, true))
+            if (EsEnteroValido(edad) && EsDecimalValido(estatura) && EsEnteroValidoDe10Digitos(telefono) &&
+                EsTextoValido(nombre) && EsTextoValido(apellidos))
             {
-                if (archivoExiste)
+                string datos = $"Nombres: {nombre}\r\nApellidos: {apellidos}\r\nTelefono: {telefono} " +
+                    $"kg\r\nEstatura: {estatura} cm\r\nEdad: {edad} años\r\nGénero: {genero}\r\n";
+
+                string rutaArchivo = "C:\\Users\\marde\\Downloads\\nada.txt";
+                bool archivoExiste = File.Exists(rutaArchivo);
+                if (archivoExiste == false)
                 {
-                    writer.WriteLine();
+                    File.WriteAllText(rutaArchivo, datos);
                 }
-                writer.WriteLine(datos);
+                else
+                {
+                    using (StreamWriter writer = new StreamWriter(rutaArchivo, true))
+                    {
+                        if (archivoExiste)
+                        {
+                            writer.WriteLine();
+                        }
+
+                        writer.WriteLine(datos);
+                        InsertarRegistro(nombre, apellidos, int.Parse(edad), genero, decimal.Parse(estatura), telefono);
+                        MessageBox.Show("Datos insertados en la Base de Datos:\n\n" + datos, "Información BD", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+
             }
-            MessageBox.Show(datos, "Valores ingresados",
-                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+            {
+                MessageBox.Show("Por favor, ingrese datos validos en los campos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool EsEnteroValido(string valor)
+        {
+            int resultado;
+            return int.TryParse(valor, out resultado);
+        }
+
+        private bool EsDecimalValido(string valor)
+        {
+            decimal resultado;
+            return decimal.TryParse(valor, out resultado);
+        }
+
+        private bool EsEnteroValidoDe10Digitos(string valor)
+        {
+            long resultado;
+            return long.TryParse(valor, out resultado) && valor.Length == 10;
+        }
+
+        private bool EsTextoValido(string valor)
+        {
+            return Regex.IsMatch(valor, @"^[a-zA-Z\s]+$"); 
+        }
+
+        private void validarEdad(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (!EsEnteroValido(textBox.Text))
+            {
+                MessageBox.Show("Por favor, ingrese una edad valida.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear();
+            }
+        }
+
+        private void validarEstatura(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (!EsDecimalValido(textBox.Text))
+            {
+                MessageBox.Show("Por favor, ingrese una estatura válida.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear();
+            }
+        }
+
+        private void validarTelefono(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            string input = textBox.Text;
+
+            if (input.Length == 10 && EsEnteroValidoDe10Digitos(input))
+            {
+                //MessageBox.Show("Por favor, ingrese un número de teléfono válido de 10 dígitos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.BackColor = Color.Green;
+                //textBox.Clear();
+            }
+            else
+            {
+                //MessageBox.Show("Por favor, ingrese un número de teléfono válido de 10 dígitos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.BackColor = Color.Red;
+            }
+        }
+
+        private void validarNombre(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (!EsTextoValido(textBox.Text))
+            {
+                MessageBox.Show("Por favor, ingrese un nombre válido (solo letras y espacios).", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear();
+            }
+        }
+
+        private void validarApellidos(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (!EsTextoValido(textBox.Text))
+            {
+                MessageBox.Show("Por favor, ingrese apellidos válidos (solo letras y espacios).", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear();
+            }
         }
 
     }
 }
-
